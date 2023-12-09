25b7cd5fe68ec62a230cc3f4e4e1bb424491c3f7
Guzz <120410710+GUS74V0@users.noreply.github.com>
2023-12-09 10:50:28 -0300

    Create LocaMais.c

---
+++ codigo/LocaMais.c
@@ -0,0 +1,11 @@
+/**
+ * @file   main.c
+ * @brief  Codigo para o Trabalho interdiciplinar de AEDS I/FES, sobre uma locadore de veiculos
+ * @author Gustavo Rodrigo - Pedro Braga - Gabriel da Silveira
+ * @date   2023-12-07
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
