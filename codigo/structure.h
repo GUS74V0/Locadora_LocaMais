15b827d18fbde447b7884029c98fbd2130e4ad9b
Guzz <120410710+GUS74V0@users.noreply.github.com>
2023-12-10 12:51:29 -0300

    Update Codigo Veiculos #4 #2

---
+++ codigo/structure.h
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+
+#ifndef ESTRUTURAS_H
+#define ESTRUTURAS_H
+
+typedef struct
+{
+    int codigo;
+    char nome[50];
+    char endereco[100];
+    char telefone[15];
+} Cliente;
+
+typedef struct
+{
+    int codigo;
+    char descricao[100];
+    char modelo[50];
+    char cor[20];
+    char placa[10];
+    float valorDiaria;
+    int qtdOcupantes;
+    char status[15];
+} Veiculo;
+
+typedef struct
+{
+    int codigo;
+    int codigoCliente;
+    int codigoVeiculo;
+    char dataRetirada[10];
+    char dataDevolucao[10];
+    int seguro; // 0= nao 1= sim
+    int qtdDias;
+} Locacao;
+
+#endif
