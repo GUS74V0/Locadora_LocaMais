a47c0d5a20f4306a2e15d020239d2903ee000968
Guzz <120410710+GUS74V0@users.noreply.github.com>
2023-12-10 13:12:37 -0300

    adiçao do menu no codigo main

--- codigo/LocaMais.c
+++ codigo/LocaMais.c
@@ -4,8 +4,79 @@
  * @author Gustavo Rodrigo - Pedro Braga - Gabriel da Silveira
  * @date   inicio 2023-12-07 | fim 2023-12-10
  */
+#include "structure.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
+
+
+
+
+
+
+
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+
+    int opcao;
+
+    do
+    {
+        printf("\n--- LocaMais ---\n");
+        printf("1. Cadastrar Cliente\n");
+        printf("2. Cadastrar Veículo\n");
+        printf("3. Cadastrar Locação\n");
+        printf("4. Dar Baixa em Locação\n");
+        printf("5. Calcular Valor Total da Locação\n");
+        printf("6. Pesquisar Cliente\n");
+        printf("7. Pesquisar Veículo\n");
+        printf("8. Exibir Locações de Cliente\n");
+        printf("9. Calcular Pontos de Fidelidade\n");
+        printf("10. Exibir Relatório de Veículos Disponíveis\n");
+        printf("0. Sair\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &opcao);
+
+        switch (opcao)
+        {
+        case 1:
+            cadastrarCliente();
+            break;
+        case 2:
+            cadastrarVeiculo();
+            break;
+        case 3:
+            cadastrarLocacao();
+            break;
+        case 4:
+            darBaixaLocacao();
+            break;
+        case 5:
+            calcularValorTotal();
+            break;
+        case 6:
+            pesquisarCliente();
+            break;
+        case 7:
+            pesquisarVeiculo();
+            break;
+        case 8:
+            exibirLocacoesCliente();
+            break;
+        case 9:
+            calcularPontosFidelidade();
+            break;
+        case 10:
+            relatorioVeiculosDisponiveis();
+            break;
+        case 0:
+            printf("Saindo do sistema...\n");
+            break;
+        default:
+            printf("Opção inválida!\n");
+        }
+    } while (opcao != 0);
+
+    return 0;
+}
